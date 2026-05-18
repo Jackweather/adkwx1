d5174ee1cb46dd10467cd83a48221ebc24d3980c
Jackweather <159730073+Jackweather@users.noreply.github.com>
2026-05-17 21:06:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -139,6 +139,7 @@ def run_task1():
         ("/opt/render/project/src/gefs_gfs/4_8_15_GFS_prate.py", "/opt/render/project/src/gefs_gfs"),
         ("/opt/render/project/src/gefs_gfs/7_11_GFS_prate.py", "/opt/render/project/src/gefs_gfs"),
         ("/opt/render/project/src/gefs_gfs/3_12_18_GFS_prate.py", "/opt/render/project/src/gefs_gfs"),
+        ('/opt/render/project/src/main_models_run/prate_type_with_gfs_euro.py', "/opt/render/project/src/main_models_run"),
     ]
     threading.Thread(target=lambda: run_scripts(scripts, 2)).start()
     return "Task started in background! Check logs folder for output.", 200
